ad937d489f4acc71ea1dc89e23d40acf09d5b219
Michael Uloth <hello@michaeluloth.com>
2024-10-16 22:47:50 -0400

    add todos

---
+++ aoc/run.py
@@ -0,0 +1,4 @@
+# TODO: https://github.com/marcelblijleven/adventofcode/blob/master/src/adventofcode/scripts/runner.py
+# TODO: https://github.com/marcelblijleven/adventofcode/blob/master/src/adventofcode/scripts/benchmarks.py
+# TODO: https://github.com/marcelblijleven/adventofcode/blob/master/src/adventofcode/scripts/generate_readme.py
+# TODO: https://github.com/xavdid/advent-of-code-python-template/blob/main/advent
