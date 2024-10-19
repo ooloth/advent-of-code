7d233bcff61d88710be8756338726fc787943ff7
Michael Uloth <hello@michaeluloth.com>
2024-10-19 18:53:17 -0400

    python: polish template's input getting helper

--- utils/py/template.py
+++ utils/py/template.py
@@ -2,8 +2,9 @@ from pathlib import Path
 
 
 def get_puzzle_input(year: int, day: int) -> str:
-    input_file = f"{Path(__file__).cwd()}/solutions/{year}/inputs/{day}.txt"
+    """Used by the python template to automatically pass the input to the solution function."""
+    input_file = Path(f"solutions/{year}/inputs/{day}.txt").resolve()
 
-    # TODO: if file is missing, create it?
+    # TODO: what if the input file is missing?
     with open(input_file) as f:
         return f.read().strip()
