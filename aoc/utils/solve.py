83bba397b3476d71bf52169571067b23937190a5
Michael Uloth <hello@michaeluloth.com>
2024-10-22 21:35:04 -0400

    solve: import solution functions more safely

--- aoc/utils/solve.py
+++ aoc/utils/solve.py
@@ -1,33 +1,46 @@
 import importlib
+from typing import Callable
 
 from rich import print
 
 from aoc.utils.cli import Day, Language, Part, Year, parse_solve_cli_args
 from aoc.utils.inputs import read_input_for_day
 
-
-
-def run_solution(year: int, day: int, part: int, language: str) -> None:
-    solution_module = importlib.import_module(
-        f"aoc.{year}.{language}.{day}{"a" if part == 1 else "b"}"
-    )
-
-    input = get_input_for_day(year, day)
-    answer = solution_module.solution(input)
-    print(f"Answer: {answer}")
 # TODO: https://github.com/marcelblijleven/adventofcode/blob/master/src/adventofcode/scripts/runner.py
 # TODO: https://github.com/marcelblijleven/adventofcode/blob/master/src/adventofcode/scripts/benchmarks.py
 # TODO: https://github.com/marcelblijleven/adventofcode/blob/master/src/adventofcode/scripts/generate_readme.py
 # TODO: https://github.com/xavdid/advent-of-code-python-template/blob/main/advent
 
 
+def get_solution_function(
+    year: Year,
+    day: Day,
+    part: Part,
+    language: Language,
+) -> Callable:
+    try:
+        module_name = f"aoc.{year}.{language}.{day}{'a' if part == 1 else 'b'}"
+        solution_module = importlib.import_module(module_name)
+        solution_function = getattr(solution_module, "solution")
+        return solution_function
+    except ModuleNotFoundError:
+        raise ImportError(f"Module {module_name} not found.")
+    except AttributeError:
+        raise ImportError(f"Function 'solution' not found in module {module_name}.")
+
+
+def get_answer(year: Year, day: Day, part: Part, language: Language) -> int:
+    solution_function = get_solution_function(year, day, part, language)
     input = read_input_for_day(year, day)
+    # print(input)
+    answer = solution_function(input)
+    return answer
 
 
 def main() -> None:
     args = parse_solve_cli_args()
-
-    run_solution(args.year, args.day, args.part, "python")
+    answer = get_answer(args.year, args.day, args.part, "python")
+    print(f"Answer: {answer}")
 
 
 # def submit_solution(year: int, day: int) -> None:
