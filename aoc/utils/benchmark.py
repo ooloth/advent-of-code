54a889f54a1bceee406c6587ba9b3e9eae592d7d
Michael Uloth <hello@michaeluloth.com>
2024-10-30 00:33:57 -0400

    clear TODOs

--- aoc/utils/benchmark.py
+++ aoc/utils/benchmark.py
@@ -1,3 +1,5 @@
 # TODO: benchmark at shell script level?
+
+# TODO: https://github.com/xavdid/advent-of-code-python-template/blob/main/advent
 # TODO: https://github.com/marcelblijleven/adventofcode/blob/master/src/adventofcode/util/helpers.py
 # TODO: https://github.com/marcelblijleven/adventofcode/blob/master/src/adventofcode/scripts/benchmarks.py
--- aoc/utils/profile.py
+++ aoc/utils/profile.py
@@ -1,2 +1,4 @@
 # TODO: profile at shell script level?
 # TODO: in addition to benchmarking runtime, profile memory and CPU usage?
+
+# TODO: https://github.com/xavdid/advent-of-code-python-template/blob/main/advent
