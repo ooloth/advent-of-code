87bf718aad31d9b364c722933f5d15a29fa28496
Michael Uloth <hello@michaeluloth.com>
2024-12-07 21:12:25 -0500

    logs: print one message

--- aoc/utils/logs.py
+++ aoc/utils/logs.py
@@ -3,6 +3,6 @@ import sys
 import rich
 
 
-def log(obj: object) -> None:
+def log(msg: str) -> None:
     """Send debugging print statements to stderr to avoid concatenating them with the puzzle answer sent to stdout."""
-    rich.print(f"{obj=}", file=sys.stderr)
+    rich.print(msg, file=sys.stderr)
