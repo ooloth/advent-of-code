4af5a950fced837bcae0fe222bbf3918066c7f20
Michael Uloth <hello@michaeluloth.com>
2024-12-09 22:04:39 -0500

    2024(7b): solution

--- aoc/2024/7b.py
+++ aoc/2024/7b.py
@@ -1,4 +1,4 @@
-"""Advent of Code 2024, Puzzle 7a: https://adventofcode.com/2024/day/7"""
+"""Advent of Code 2024, Puzzle 7b: https://adventofcode.com/2024/day/7"""
 
 from itertools import product
 
@@ -11,6 +11,7 @@ TotalCalibrationResult = int  # sum of test values from the equations that can b
 
 
 def parse_input(input: str) -> ResultsAndEquationNumbers:
+    """Same as 7a."""
     return {
         int(test_value): list(map(int, equation_numbers.split(" ")))  # 3
         for line in input.splitlines()  # 1
@@ -19,9 +20,15 @@ def parse_input(input: str) -> ResultsAndEquationNumbers:
 
 
 def can_add_or_multiply_to_target(target: int, numbers: list[int]) -> bool:
-    """Return True if the numbers can be added or multiplied to the target, False otherwise."""
-    operators = ["+", "*"]
+    """
+    Return True if the numbers can be added or multiplied to the target, including when neighbouring
+    numbers are concatenated.
+    """
+    operators = ["+", "*", "||"]
     possible_operator_sequences = product(operators, repeat=len(numbers) - 1)
+
+    def concatenate(a: int, b: int) -> int:
+        return int(str(a) + str(b))
 
     for operator_list in possible_operator_sequences:
         result = numbers[0]
@@ -32,6 +39,8 @@ def can_add_or_multiply_to_target(target: int, numbers: list[int]) -> bool:
                 result += number
             elif operator == "*":
                 result *= number
+            elif operator == "||":
+                result = concatenate(result, number)
 
         if result == target:
             return True
@@ -39,6 +48,7 @@ def can_add_or_multiply_to_target(target: int, numbers: list[int]) -> bool:
 
 
 def keep_solvable_equations(test_values_and_equations: ResultsAndEquationNumbers) -> list[CalibrationResult]:
+    """Same as 7a."""
     return [
         value  # 3
         for value, equation_numbers in test_values_and_equations.items()  # 1
@@ -65,7 +75,7 @@ def test_solution() -> None:
 192: 17 8 14
 21037: 9 7 18 13
 292: 11 6 16 20"""
-    example_answer: TotalCalibrationResult = 3749
+    example_answer: TotalCalibrationResult = 11387
 
     assert solution(example_input) == example_answer
 
